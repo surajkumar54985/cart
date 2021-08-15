164c1165c0c5446ff1c2a38cf52edb6319cc5478
Suraj kumar <surajkumar54985@gmail.com>
2021-08-15 19:36:25 +0530

    Add increase quantity function

--- src/CartItem.js
+++ src/CartItem.js
@@ -6,26 +6,50 @@ class Cartitem extends React.Component {
     super();
     this.state = {
       price : 999,
-      title : 'phone',
+      title : 'Mobile phone',
       qty : 1,
-      img : ''
+      img : '' 
     }
+    // this.increaseQuantity = this.increaseQuantity.bind(this);
   }
+  increaseQuantity = () => {
+    console.log('this' , this.state);
+  }
+  // decreaseQuantity (){
+  //   console.log('test');
+  // }
     render() {
+        const {price,title,qty} = this.state;
         return (
             <div className="cart-item">
               <div className="left-block">
                 <img style={styles.image} />
               </div>
               <div className="right-block">
-                <div style={{fontSize: 25}}>Phone</div>
-                <div style={{color: '#777'}}>Rs 999</div>
-                <div style={{color: '#777'}}>Qty : 1</div>
+                <div style={{fontSize: 25}}>{title}</div>
+                <div style={{color: '#777'}}>Rs {price}</div>
+                <div style={{color: '#777'}}>Qty: {qty}</div>
                 <div className="cart-item-actions">
                   {/* Buttons */}
-                  <img alt="increase" className="action-icons" src="https://stock.adobe.com/images/plus-icon-add-plus-icon-addition-sign-medical-plus-icon/251038282" />
-                  <img alt="decrease" className="action-icons" src="https://as1.ftcdn.net/v2/jpg/03/73/49/86/500_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg" />
-                  <img alt="delete" className="action-icons" src="https://as2.ftcdn.net/v2/jpg/00/98/26/11/500_F_98261175_Sv69O3rZsHApYkjAdrWbgQixYHwyZyOr.jpg" />
+                  <img 
+                    alt="increase" 
+                    className="action-icons" 
+                    src="https://image.flaticon.com/icons/png/512/992/992651.png" 
+                    onClick={this.increaseQuantity.bind(this)}
+                  />
+
+                  <img
+                    alt="decrease" 
+                    className="action-icons" 
+                    src="https://as1.ftcdn.net/v2/jpg/03/73/49/86/500_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg" 
+                    onClick={this.decreaseQuantity}
+                  />
+                  <img
+                    alt="delete" 
+                    className="action-icons" 
+                    src="https://as2.ftcdn.net/v2/jpg/00/98/26/11/500_F_98261175_Sv69O3rZsHApYkjAdrWbgQixYHwyZyOr.jpg" 
+                    onClick={this.deleteItem}
+                  />
                 </div>
               </div>
             </div>
